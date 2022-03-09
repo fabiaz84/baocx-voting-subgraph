c5d41d3c26afd7ef9435fbd3f96626fdebc8abea
fabiaz84 <79611566+fabiaz84@users.noreply.github.com>
2022-03-10 00:04:40 +0100

    mainnet

--- src/holdersBao.ts
+++ src/holdersBao.ts
@@ -2,9 +2,6 @@ import { BigInt, Address, store } from '@graphprotocol/graph-ts';
 import { Transfer } from '../generated/BaoToken/BaoToken';
 import { HolderBao } from '../generated/schema';
 
-export const BIG_INT_ONE_HUNDRED = BigInt.fromI32(100);
-export const BIG_INT_TWENTY_FIVE = BigInt.fromI32(25);
-
 function updateBalance(tokenAddress: Address, holderAddress: Address, value: BigInt, increase: boolean): void {
   if (holderAddress.toHexString() == '0x0000000000000000000000000000000000000000') return;
   let id = tokenAddress.toHex() + '-' + holderAddress.toHex();
@@ -15,14 +12,10 @@ function updateBalance(tokenAddress: Address, holderAddress: Address, value: Big
     holder.balance = BigInt.fromI32(0);
   } 
     holder.balance = increase ? holder.balance.plus(value) : holder.balance.minus(value);
-        if (holder.balance.isZero()) {
-    store.remove('HolderBao', id);
-  } else {
     holder.save();
-  }
 }
 
 export function handleTransferBao(event: Transfer): void {
-  updateBalance(event.address, event.params.from, event.params.value.times(BIG_INT_TWENTY_FIVE).div(BIG_INT_ONE_HUNDRED), false);
-  updateBalance(event.address, event.params.to, event.params.value.times(BIG_INT_TWENTY_FIVE).div(BIG_INT_ONE_HUNDRED), true);
+  updateBalance(event.address, event.params.from, event.params.value, false);
+  updateBalance(event.address, event.params.to, event.params.value, true);
 }
